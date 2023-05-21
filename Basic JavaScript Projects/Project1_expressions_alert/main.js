fdaeb655120a1aa46de9510a95c77f086db86ff7
JamesSanta <jamessanta@hotmail.com>
2023-05-21 08:49:37 -0400

    JS added

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,9 @@
+ var welcome="Welcome to my homepage";  // Creating a variable
+
+    window.alert(welcome);
+
+var Sent1= "This is the beginning of the string"; // The first variable
+var Sent2= " and this is the end of the string. "; // The second variable
+document.write(Sent1 + Sent2); // Concatenating the two variables.
+
+document.write(3 + 3); //Addition expression
