7ce26000a6b0957754382ba9e345520341fa1cd2
JamesSanta <jamessanta@hotmail.com>
2023-05-25 22:13:22 -0400

    New JS code

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/example.js
@@ -0,0 +1,9 @@
+function my_function() {
+  //  document.getElementById("Test").innerHTML = 0/0;
+  //  document.getElementById("Test").innerHTML = isNaN('This is a string');
+  document.getElementById("Test").innerHTML = isNaN('007');
+}
+
+document.write(2e310);
+document.write(-3e310);
+
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,25 @@
+document.write(typeof "word" + "<br>"); // Type of operator for a string
+document.write("212" + 212 + "<br>"); // Combining a string and a number
+// document.write(12>21);
+// document.write(234>200);
+// console.log(200 + 2 - 72);
+// console.log(222<=19);
+// document.write(12 == 10);
+document.write(12 + 12 == 24); // Using equals to operator
+
+x = 24;
+// y = "Hello, nurse";
+z = "Twenty Four";
+// c = "charlie";
+// d = "charlie";
+// document.write(x === y);
+document.write(x === z); // Strict equal operator
+// document.write(c === d);
+// document.write(z === y);
+// document.write(10 > 12 && 12 > 10);
+document.write(22 >= 22 && 43 > 34); // And operator 
+// document.write(10 > 12 || 12 > 10);
+document.write(12 >= 13 || 13 <= 12);
+ function not_function() {
+   document.getElementById("Not").innerHTML = !(20 < 10); // Not function in operation
+ }
